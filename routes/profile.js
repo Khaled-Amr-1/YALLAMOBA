5a3dbc61d85f28d756191039716eb6746fe27432
Khaled Amr <khaledamr520@gmail.com>
2025-04-30 21:27:54 +0300

    profile first

--- routes/profile.js
+++ routes/profile.js
@@ -4,11 +4,27 @@ import pool from "../config/db.js"; // Import the pool from the config
 import dotenv from "dotenv";
 dotenv.config();
 
-router.get("/test", async (req, res) => {
-    res.json({ message: "This is a test route" });
-  }
-);
-
+router.get("/profile/:uid", async (req, res) => {
+    const { uid } = req.params;
+    if (!uid || typeof uid !== "number") {
+        return res.status(400).json({ error: "ENTER VALID ID" });
+    }
+    
+    try {
+        const result = await pool.query(
+        "SELECT username, gender, role, avatar, uid, popularity FROM users WHERE uid = $1",
+        [uid]
+        );
+        if (result.rows.length === 0) {
+        return res.status(404).json({ error: "User not found" });
+        }
+        const user = result.rows[0];
+        res.status(200).json(user);
+    } catch (error) {
+        console.error("Error fetching user profile:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
 
 export default router;
 
