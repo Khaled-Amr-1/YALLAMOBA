037f219e298fe5f8ec8c09833f561d1463940465
Khaled Amr <khaledamr520@gmail.com>
2025-04-30 21:19:41 +0300

    router profile

--- routes/profile.js
+++ routes/profile.js
@@ -4,7 +4,7 @@ import pool from "../config/db.js"; // Import the pool from the config
 import dotenv from "dotenv";
 dotenv.config();
 
-router.get("test", async (req, res) => {
+router.get("/test", async (req, res) => {
     res.json({ message: "This is a test route" });
   }
 );
